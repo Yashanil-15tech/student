0f2efcd33cc3c571f90b0d21131f0b0844ea95c2
Yashanil-15tech <212117yashaniltiwari@gmail.com>
2026-04-15 00:02:04 +0530

    Initial commit

--- student_portal/config/wsgi.py
+++ student_portal/config/wsgi.py
@@ -1,11 +1,11 @@
-"""
-WSGI config for Student Portal project.
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+import django
+django.setup()
+from django.core.management import call_command
+call_command('migrate', verbosity=0)  # no --run-syncdb needed for postgres
 
 application = get_wsgi_application()
